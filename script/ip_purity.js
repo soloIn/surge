6d730f23a31a017efb2e41bc1cb78bbe8c7f3707
pong solo <solor1664931260@outlook.com>
2026-04-20 13:11:11 +0800

    Update ip_purity.js

--- script/ip_purity.js
+++ script/ip_purity.js
@@ -69,6 +69,7 @@ function probeViaPolicy(url, policyName, extraHeaders = {}) {
             timeout: 8 // 单个节点的请求超时设为 8 秒，避免长时间卡死
         }, (error, response, data) => {
             const rtt = Date.now() - startTime;
+            console.log("res:" + response)
             if (error || !(response && response.status === 200)) {
                 resolve({ success: false, policy: policyName, error: error });
             } else {
